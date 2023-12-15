f79b0278e9f64d04cf53a6aa0707c0c37a5a16ea
Fauzan Ardhana <fauzanardh91@gmail.com>
2023-12-15 06:36:48 +0000

    remove mask, causal, and attention bias and use conv1d instead of linear

--- modules/attention.py
+++ modules/attention.py
@@ -12,15 +12,13 @@ _config = namedtuple("Config", ["enable_flash", "enable_math", "enable_mem_effic
 
 
 class Attention(nn.Module):
-    def __init__(self: "Attention", dropout: float = 0.0, causal: bool = False, sdpa: bool = False) -> None:
+    def __init__(self: "Attention", dropout: float = 0.0, sdpa: bool = False) -> None:
         super().__init__()
         self.dropout = dropout
         self.attn_dropout = nn.Dropout(dropout)
-        self.causal = causal
 
         assert not (sdpa and version.parse(torch.__version__) < version.parse("2.0.0")), "sdpa requires torch>=2.0.0"
         self.sdpa = sdpa
-        self.register_buffer("mask", None, persistent=False)
 
         # sdpa configs
         self.cpu_config = _config(True, True, True)
@@ -41,24 +39,11 @@ class Attention(nn.Module):
         q: torch.Tensor,
         k: torch.Tensor,
         v: torch.Tensor,
-        mask: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
-        _, heads, q_len, _, k_len, is_cuda, dtype = (
-            *q.shape,
-            k.shape[-2],
-            q.is_cuda,
-            q.dtype,
-        )
+        is_cuda, dtype = q.is_cuda, q.dtype
 
         k = rearrange(k, "b ... -> b 1 ...").expand_as(q)
         v = rearrange(v, "b ... -> b 1 ...").expand_as(q)
-
-        if mask is not None:
-            mask = rearrange(mask, "b j -> b 1 1 j")
-            mask = mask.expand(-1, heads, q_len, -1)
-            if self.causal:
-                causal_mask = torch.ones((q_len, k_len), device=q.device, dtype=torch.bool).triu(k_len - q_len + 1)
-                mask = mask & ~causal_mask
 
         config = self.cuda_config if is_cuda else self.cpu_config
 
@@ -73,7 +58,6 @@ class Attention(nn.Module):
                 q,
                 k,
                 v,
-                attn_mask=mask,
                 dropout_p=self.dropout if self.training else 0.0,
             )
 
@@ -84,27 +68,12 @@ class Attention(nn.Module):
         q: torch.Tensor,
         k: torch.Tensor,
         v: torch.Tensor,
-        mask: Optional[torch.Tensor] = None,
-        attn_bias: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         if self.sdpa:
-            assert attn_bias is None, "sdpa does not support attn_bias"
-            return self.sdpa_attn(q, k, v, mask=mask)
+            return self.sdpa_attn(q, k, v)
 
         scale = q.shape[-1] ** -0.5
         sim = einsum("b h i d, b h j d -> b h i j", q, k) * scale
-
-        if attn_bias is not None:
-            sim += attn_bias
-
-        if mask is not None:
-            mask = rearrange(mask, "b j -> b 1 1 j")
-            sim = sim.masked_fill(~mask, -torch.finfo(sim.dtype).max)
-
-        if self.causal:
-            i, j = sim.shape[-2:]
-            causal_mask = torch.ones((i, j), device=sim.device, dtype=torch.bool).triu(j - i + 1)
-            sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
 
         attn = sim.softmax(dim=-1)
         attn = self.attn_dropout(attn)
@@ -120,27 +89,25 @@ class MultiHeadAttention(nn.Module):
         context_dim: Optional[int] = None,
         dim_head: int = 64,
         heads: int = 8,
-        causal: bool = False,
         dropout: float = 0.25,
         sdpa: bool = False,
         is_cross_attention: bool = False,
     ) -> None:
         super().__init__()
         self.heads = heads
-        self.causal = causal
         self.is_cross_attention = is_cross_attention
 
         inner_dim = dim_head * heads
 
         if is_cross_attention:
             assert context_dim is not None, "context_dim must be provided for cross attention"
-            self.to_q = nn.Linear(dim, inner_dim, bias=False)
-            self.to_kv = nn.Linear(context_dim, inner_dim * 2, bias=False)
+            self.to_q = nn.Conv1d(dim, inner_dim, 1, bias=False)
+            self.to_kv = nn.Conv1d(context_dim, inner_dim * 2, 1, bias=False)
         else:
-            self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
-        self.attention = Attention(dropout=dropout, causal=causal, sdpa=sdpa)
+            self.to_qkv = nn.Conv1d(dim, inner_dim * 3, 1, bias=False)
+        self.attention = Attention(dropout=dropout, sdpa=sdpa)
         self.to_out = nn.Sequential(
-            nn.Linear(inner_dim, dim),
+            nn.Conv1d(inner_dim, dim, 1, bias=False),
             nn.Dropout(dropout),
         )
 
@@ -148,22 +115,20 @@ class MultiHeadAttention(nn.Module):
         self: "MultiHeadAttention",
         x: torch.Tensor,
         context: Optional[torch.Tensor] = None,
-        mask: Optional[torch.Tensor] = None,
-        attn_bias: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         if self.is_cross_attention:
             assert context is not None, "context must be provided for cross attention"
             q = self.to_q(x)
-            k, v = self.to_kv(context).chunk(2, dim=-1)
+            k, v = self.to_kv(context).chunk(2, dim=-2)
         else:
-            q, k, v = self.to_qkv(x).chunk(3, dim=-1)
+            q, k, v = self.to_qkv(x).chunk(3, dim=-2)
 
-        q = rearrange(q, "b n (h d) -> b h n d", h=self.heads)
-        k = rearrange(k, "b n (h d) -> b h n d", h=self.heads)
-        v = rearrange(v, "b n (h d) -> b h n d", h=self.heads)
+        q = rearrange(q, "b (h d) n -> b h n d", h=self.heads)
+        k = rearrange(k, "b (h d) n -> b h n d", h=self.heads)
+        v = rearrange(v, "b (h d) n -> b h n d", h=self.heads)
 
-        out = self.attention(q, k, v, mask=mask, attn_bias=attn_bias)
-        out = rearrange(out, "b h n d -> b n (h d)")
+        out = self.attention(q, k, v)
+        out = rearrange(out, "b h n d -> b (h d) n")
 
         out = self.to_out(out)
         return out
