fdd8b3e3d2130167e6c7e2a4b790428d99352183
Fauzan Ardhana <fauzanardh91@gmail.com>
2023-12-16 03:18:49 +0000

    add transformer and unet architecture

---
+++ modules/transformer.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn
+from einops import rearrange, repeat
+
+from modules.attention import MultiHeadAttention
+
+
+class FeedForward(nn.Sequential):
+    def __init__(
+        self: "FeedForward",
+        dim: int,
+        dim_mult: int = 4,
+        dropout: float = 0.25,
+    ) -> None:
+        inner_dim = dim * dim_mult
+        super().__init__(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, inner_dim),
+            nn.ReLU(inplace=True),
+            nn.LayerNorm(inner_dim),
+            nn.Dropout(dropout),
+            nn.Linear(inner_dim, dim),
+        )
+
+
+class TransformerBlock(nn.Module):
+    def __init__(
+        self: "TransformerBlock",
+        dim: int,
+        dim_context: int,
+        dim_head: int = 32,
+        heads: int = 8,
+        dropout: float = 0.25,
+        sdpa: bool = True,
+        ff_dropout: float = 0.25,
+    ) -> None:
+        super().__init__()
+        self.self_attention = MultiHeadAttention(
+            dim,
+            dim_head=dim_head,
+            heads=heads,
+            dropout=dropout,
+            sdpa=sdpa,
+        )
+        self.feed_forward = FeedForward(dim, dropout=ff_dropout)
+        self.cross_attention = MultiHeadAttention(
+            dim,
+            dim_context=dim_context,
+            dim_head=dim_head,
+            heads=heads,
+            dropout=dropout,
+            sdpa=sdpa,
+            is_cross_attention=True,
+        )
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.norm3 = nn.LayerNorm(dim)
+
+    def forward(self: "TransformerBlock", x: torch.Tensor, context: torch.Tensor) -> torch.Tensor:
+        x = x + self.self_attention(self.norm1(x))
+        x = x + self.cross_attention(self.norm2(x), context)
+        x = x + self.feed_forward(self.norm3(x))
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self: "Transformer",
+        dim: int,
+        dim_context: int,
+        dim_head: int = 32,
+        heads: int = 8,
+        depth: int = 4,
+        dropout: float = 0.25,
+        sdpa: bool = True,
+        ff_dropout: float = 0.25,
+    ) -> None:
+        super().__init__()
+        self.layers = nn.ModuleList(
+            [
+                TransformerBlock(
+                    dim,
+                    dim_context,
+                    dim_head=dim_head,
+                    heads=heads,
+                    dropout=dropout,
+                    sdpa=sdpa,
+                    ff_dropout=ff_dropout,
+                )
+                for _ in range(depth)
+            ],
+        )
+
+    def forward(self: "Transformer", x: torch.Tensor, context: torch.Tensor) -> torch.Tensor:
+        x = rearrange(x, "b d n -> b n d")
+        for layer in self.layers:
+            c = repeat(context, "b d -> b n d", n=x.shape[1])
+            x = layer(x, c)
+        x = rearrange(x, "b n d -> b d n")
+        return x
---
+++ modules/unet.py
@@ -0,0 +1,241 @@
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+
+from modules.causal_convolution import CausalConv1d, CausalConvTranspose1d
+from modules.residual import ResidualBlockV2
+from modules.transformer import Transformer
+from modules.utils import timestep_embedding
+
+
+class ResnetBlock(nn.Module):
+    def __init__(
+        self: "ResnetBlock",
+        dim_in: int,
+        dim_out: int,
+        dim_emb: int,
+        dim_context: int,
+        dilations: Tuple[int] = (1, 3, 9),
+        kernel_size: int = 7,
+        squeeze_excite: bool = True,
+        attn_dim_head: int = 32,
+        attn_heads: int = 8,
+        attn_dropout: float = 0.25,
+        attn_sdpa: bool = True,
+    ) -> None:
+        super().__init__()
+
+        self.residual_blocks = nn.ModuleList(
+            [
+                *[
+                    ResidualBlockV2(
+                        dim_in,
+                        dim_in,
+                        dim_emb,
+                        dim_context,
+                        dilation,
+                        kernel_size=kernel_size,
+                        squeeze_excite=squeeze_excite,
+                        attn_dim_head=attn_dim_head,
+                        attn_heads=attn_heads,
+                        attn_dropout=attn_dropout,
+                        attn_sdpa=attn_sdpa,
+                    )
+                    for dilation in dilations[:-1]
+                ],
+                ResidualBlockV2(
+                    dim_in,
+                    dim_out,
+                    dim_emb,
+                    dim_context,
+                    dilations[-1],
+                    kernel_size=kernel_size,
+                    squeeze_excite=squeeze_excite,
+                    attn_dim_head=attn_dim_head,
+                    attn_heads=attn_heads,
+                    attn_dropout=attn_dropout,
+                    attn_sdpa=attn_sdpa,
+                ),
+            ],
+        )
+
+    def forward(self: "ResnetBlock", x: torch.Tensor, t_emb: torch.Tensor, context: torch.Tensor) -> torch.Tensor:
+        for residual_block in self.residual_blocks:
+            x = residual_block(x, t_emb, context)
+
+        return x
+
+
+class Unet(nn.Module):
+    def __init__(
+        self: "Unet",
+        dim_in: int,
+        dim_out: int,
+        dim_h: int,
+        dim_cond: int,
+        dim_h_mult: Tuple[int] = (1, 2, 4, 8),
+        res_strides: Tuple[int] = (2, 2, 2, 2),
+        res_dilations: Tuple[int] = (1, 3, 9),
+        attn_dim_head: int = 32,
+        attn_heads: int = 8,
+        attn_depth: int = 4,
+        attn_dropout: float = 0.25,
+        attn_sdpa: bool = True,
+        attn_ff_dropout: float = 0.25,
+    ) -> None:
+        super().__init__()
+        self.dim_h = dim_h
+        self.dim_emb = dim_h * 4
+
+        self.pre_conv = CausalConv1d(dim_in, dim_h, 7)
+        self.final_conv = CausalConv1d(dim_h, dim_out, 1)
+        self.time_embedding = nn.Sequential(
+            nn.Linear(dim_h, self.dim_emb),
+            nn.ReLU(inplace=True),
+            nn.Linear(self.dim_emb, self.dim_emb),
+        )
+
+        # Downsample
+        dims_h = tuple((dim_h * mult) for mult in dim_h_mult)
+        dims_h = (dim_h, *dims_h)
+        in_out = tuple(zip(dims_h[:-1], dims_h[1:]))
+        n_layers = len(in_out)
+
+        skip_connection_dims = []
+        down_layers = []
+        for i in range(n_layers):
+            layer_dim_in, layer_dim_out = in_out[i]
+            stride = res_strides[i]
+            down_layers.append(
+                nn.ModuleList(
+                    [
+                        ResnetBlock(
+                            layer_dim_in,
+                            layer_dim_in,
+                            self.dim_emb,
+                            dim_cond,
+                            dilations=res_dilations,
+                            squeeze_excite=i == n_layers - 1,
+                            attn_dim_head=attn_dim_head,
+                            attn_heads=attn_heads,
+                            attn_dropout=attn_dropout,
+                            attn_sdpa=attn_sdpa,
+                        ),
+                        Transformer(
+                            layer_dim_in,
+                            dim_cond,
+                            dim_head=attn_dim_head,
+                            heads=attn_heads,
+                            depth=attn_depth,
+                            dropout=attn_dropout,
+                            sdpa=attn_sdpa,
+                            ff_dropout=attn_ff_dropout,
+                        ),
+                        CausalConv1d(layer_dim_in, layer_dim_out, 2 * stride, stride=stride),
+                    ],
+                ),
+            )
+            skip_connection_dims.append(layer_dim_in)
+        self.down_layers = nn.ModuleList(down_layers)
+
+        # Middle
+        self.middle_resnet1 = ResnetBlock(
+            dims_h[-1],
+            dims_h[-1],
+            self.dim_emb,
+            dim_cond,
+            dilations=res_dilations,
+            squeeze_excite=False,
+            attn_dim_head=attn_dim_head,
+            attn_heads=attn_heads,
+            attn_dropout=attn_dropout,
+            attn_sdpa=attn_sdpa,
+        )
+        self.middle_transformer = Transformer(
+            dims_h[-1],
+            dim_cond,
+            dim_head=attn_dim_head,
+            heads=attn_heads,
+            depth=attn_depth,
+            dropout=attn_dropout,
+            sdpa=attn_sdpa,
+            ff_dropout=attn_ff_dropout,
+        )
+        self.middle_resnet2 = ResnetBlock(
+            dims_h[-1],
+            dims_h[-1],
+            self.dim_emb,
+            dim_cond,
+            dilations=res_dilations,
+            squeeze_excite=False,
+            attn_dim_head=attn_dim_head,
+            attn_heads=attn_heads,
+            attn_dropout=attn_dropout,
+            attn_sdpa=attn_sdpa,
+        )
+
+        # Upsample
+        res_strides = tuple(reversed(res_strides))
+        in_out = tuple(reversed(tuple(zip(dims_h[:-1], dims_h[1:]))))
+        n_layers = len(in_out)
+
+        up_layers = []
+        for i in range(n_layers):
+            layer_dim_in, layer_dim_out = in_out[i]
+            skip_connection_dim = skip_connection_dims.pop()
+            stride = res_strides[i]
+            up_layers.append(
+                nn.ModuleList(
+                    [
+                        CausalConvTranspose1d(layer_dim_out, layer_dim_in, 2 * stride, stride=stride),
+                        ResnetBlock(
+                            layer_dim_in + skip_connection_dim,
+                            layer_dim_in,
+                            self.dim_emb,
+                            dim_cond,
+                            dilations=res_dilations,
+                            squeeze_excite=i == 0,
+                            attn_dim_head=attn_dim_head,
+                            attn_heads=attn_heads,
+                            attn_dropout=attn_dropout,
+                            attn_sdpa=attn_sdpa,
+                        ),
+                        Transformer(
+                            layer_dim_in,
+                            dim_cond,
+                            dim_head=attn_dim_head,
+                            heads=attn_heads,
+                            depth=attn_depth,
+                            dropout=attn_dropout,
+                            sdpa=attn_sdpa,
+                            ff_dropout=attn_ff_dropout,
+                        ),
+                    ],
+                ),
+            )
+        self.up_layers = nn.ModuleList(up_layers)
+
+    def forward(self: "Unet", x: torch.Tensor, t: torch.Tensor, c: torch.Tensor) -> torch.Tensor:
+        x = self.pre_conv(x)
+
+        t_emb = timestep_embedding(t, self.dim_h)
+        t_emb = self.time_embedding(t_emb).squeeze(1)
+
+        skip_connections = []
+        for down_resnet, down_transformer, downsample in self.down_layers:
+            x = down_resnet(x, t_emb, c)
+            skip_connections.append(x)
+            x = down_transformer(x, c)
+            x = downsample(x)
+
+        x = self.middle_resnet1(x, t_emb, c)
+        x = self.middle_transformer(x, c)
+        x = self.middle_resnet2(x, t_emb, c)
+
+        for upsample, up_resnet, up_transformer in self.up_layers:
+            x = upsample(x)
+            x = up_resnet(torch.cat([x, skip_connections.pop()], dim=1), t_emb, c)
+            x = up_transformer(x, c)
+
+        return self.final_conv(x)
