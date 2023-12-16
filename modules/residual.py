1bafb8332577c7f5b438dfc240890a2f346fe20c
Fauzan Ardhana <fauzanardh91@gmail.com>
2023-12-16 03:09:00 +0000

    add new residual block architecture

--- modules/residual.py
+++ modules/residual.py
@@ -1,9 +1,10 @@
-from typing import Dict
+from typing import Optional
 
 import torch
 import torch.nn as nn
-from einops import rearrange
+from einops import rearrange, repeat
 
+from modules.attention import MultiHeadAttention
 from modules.causal_convolution import CausalConv1d
 
 
@@ -28,34 +29,123 @@ class SqueezeExcite(nn.Module):
         return x * self.layers(mean)
 
 
-class Residual(nn.Module):
-    def __init__(self: "Residual", fn: callable) -> None:
-        super().__init__()
-        self.fn = fn
-
-    def forward(self: "Residual", x: torch.Tensor, **kwargs: Dict) -> torch.Tensor:
-        return x + self.fn(x, **kwargs)
-
-
 class ResidualBlock(nn.Module):
     def __init__(
         self: "ResidualBlock",
         dim_in: int,
         dim_out: int,
+        dim_emb: int,
         dilation: int,
         kernel_size: int = 7,
         squeeze_excite: bool = True,
     ) -> None:
         super().__init__()
-        self.layers = Residual(
-            nn.Sequential(
-                CausalConv1d(dim_in, dim_out, kernel_size, dilation=dilation),
-                nn.ReLU(inplace=True),
-                CausalConv1d(dim_out, dim_out, 1),
-                nn.ReLU(inplace=True),
-                SqueezeExcite(dim_out) if squeeze_excite else nn.Identity(),
-            ),
+
+        self.emb = nn.Sequential(
+            nn.SiLU(inplace=True),
+            nn.Linear(dim_emb, dim_in),
+        )
+        self.layers = nn.Sequential(
+            CausalConv1d(dim_in, dim_out, kernel_size, dilation=dilation),
+            nn.ReLU(inplace=True),
+            CausalConv1d(dim_out, dim_out, 1),
+            nn.ReLU(inplace=True),
+            SqueezeExcite(dim_out) if squeeze_excite else nn.Identity(),
+        )
+        self.res_conv = nn.Conv1d(dim_in, dim_out, 1) if dim_in != dim_out else nn.Identity()
+
+    def forward(self: "ResidualBlock", x: torch.Tensor, time_emb: torch.Tensor) -> torch.Tensor:
+        h = x
+
+        time_emb = self.emb(time_emb).unsqueeze(-1)
+        h = self.layers(h + time_emb)
+
+        return h + self.res_conv(x)
+
+
+class Block(nn.Module):
+    def __init__(
+        self: "Block",
+        dim_in: int,
+        dim_out: int,
+        dilation: int,
+        norm: bool = True,
+        kernel_size: int = 7,
+        squeeze_excite: bool = True,
+    ) -> None:
+        super().__init__()
+        self.norm = nn.GroupNorm(1, dim_in) if norm else nn.Identity()
+        self.layers = nn.Sequential(
+            CausalConv1d(dim_in, dim_out, kernel_size, dilation=dilation),
+            nn.ReLU(inplace=True),
+            CausalConv1d(dim_out, dim_out, 1),
+            nn.ReLU(inplace=True),
+            SqueezeExcite(dim_out) if squeeze_excite else nn.Identity(),
         )
 
-    def forward(self: "ResidualBlock", x: torch.Tensor) -> torch.Tensor:
-        return self.layers(x)
+    def forward(self: "Block", x: torch.Tensor, scale_shift: Optional[torch.Tensor] = None) -> torch.Tensor:
+        x = self.norm(x)
+
+        if scale_shift is not None:
+            scale, shift = scale_shift
+            x = x * (scale + 1) + shift
+
+        x = self.layers(x)
+        return x
+
+
+class ResidualBlockV2(nn.Module):
+    def __init__(
+        self: "ResidualBlockV2",
+        dim_in: int,
+        dim_out: int,
+        dim_emb: int,
+        dim_context: int,
+        dilation: int,
+        kernel_size: int = 7,
+        squeeze_excite: bool = True,
+        attn_dim_head: int = 32,
+        attn_heads: int = 8,
+        attn_dropout: float = 0.25,
+        attn_sdpa: bool = True,
+    ) -> None:
+        super().__init__()
+
+        self.time_mlp = nn.Sequential(
+            nn.SiLU(inplace=True),
+            nn.Linear(dim_emb, dim_out * 2),
+        )
+        self.cross_attention = MultiHeadAttention(
+            dim_out,
+            dim_context=dim_context,
+            dim_head=attn_dim_head,
+            heads=attn_heads,
+            dropout=attn_dropout,
+            sdpa=attn_sdpa,
+            is_cross_attention=True,
+        )
+        self.block1 = Block(dim_in, dim_out, dilation, kernel_size=kernel_size, squeeze_excite=squeeze_excite)
+        self.block2 = Block(dim_out, dim_out, dilation, kernel_size=kernel_size, squeeze_excite=squeeze_excite)
+
+        self.res_conv = CausalConv1d(dim_in, dim_out, 1) if dim_in != dim_out else nn.Identity()
+
+    def forward(
+        self: "ResidualBlockV2",
+        x: torch.Tensor,
+        time_emb: torch.Tensor,
+        context: torch.Tensor,
+    ) -> torch.Tensor:
+        time_emb = self.time_mlp(time_emb)
+        time_emb = rearrange(time_emb, "b n -> b n 1")
+        scale_shift = time_emb.chunk(2, dim=1)
+
+        h = self.block1(x)
+
+        h = rearrange(h, "b d n -> b n d")
+        context = repeat(context, "b d -> b n d", n=h.shape[-1])
+        h = self.cross_attention(h, context) + h
+        h = rearrange(h, "b n d -> b d n")
+
+        h = self.block2(h, scale_shift=scale_shift)
+
+        return h + self.res_conv(x)
