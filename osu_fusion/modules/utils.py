cb96d2f29b980ed513c0d55592475780dd0fbe89
Fauzan Ardhana <fauzanardh91@gmail.com>
2024-05-07 11:01:21 +0700

    enable jit on rotary pos emb again

--- osu_fusion/modules/utils.py
+++ osu_fusion/modules/utils.py
@@ -24,7 +24,7 @@ def rotate_half(x: torch.Tensor) -> torch.Tensor:
     return torch.cat((-x2, x1), dim=-1)
 
 
-# @torch.jit.script
+@torch.jit.script
 def apply_rotary_pos_emb(x: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor) -> torch.Tensor:
     cos = cos[:, :, : x.shape[-2], :]
     sin = sin[:, :, : x.shape[-2], :]
