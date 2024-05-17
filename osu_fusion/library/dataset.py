ebd8cc59905b5f44dbe2a9bc8fca98e86987bcaa
Fauzan Ardhana <fauzanardh91@gmail.com>
2024-05-18 04:00:25 +0700

    Remove MAX_LENGTH for FullSequenceDataset since it's no longer a concern

--- osu_fusion/library/dataset.py
+++ osu_fusion/library/dataset.py
@@ -94,15 +94,8 @@ class DummyDataset(StreamPerSample):
 
 
 class FullSequenceDataset(StreamPerSample):
-    MAX_LENGTH = 65536
-
     def sample_stream(self: StreamPerSample, map_file: Path) -> Generator[torch.Tensor, None, None]:
-        x, a, c = load_tensor(map_file)
-
-        if x.shape[-1] > self.MAX_LENGTH:
-            return
-
-        yield x[..., : self.MAX_LENGTH], a[..., : self.MAX_LENGTH], c
+        yield load_tensor(map_file)
 
 
 class SubsequenceDataset(StreamPerSample):
