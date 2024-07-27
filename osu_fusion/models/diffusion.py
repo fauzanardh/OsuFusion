8c7e575330896fee40b65110f6e17a4f85c69848
Fauzan Ardhana <fauzanardh91@gmail.com>
2024-07-28 01:37:07 +0700

    fix typo

--- osu_fusion/models/diffusion.py
+++ osu_fusion/models/diffusion.py
@@ -58,7 +58,7 @@ class OsuFusion(nn.Module):
         self.scheduler = DDIMScheduler(
             num_train_timesteps=train_timesteps,
             beta_schedule="linear",
-            clip_samples=False,
+            clip_sample=False,
             rescale_betas_zero_snr=True,
         )
         self.train_timesteps = train_timesteps
