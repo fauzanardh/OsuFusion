212d8ff0b807297b6662a21944215722c2707b84
Fauzan Ardhana <fauzanardh91@gmail.com>
2024-08-10 00:25:23 +0700

    change kernel size for the out layer

--- osu_fusion/modules/mmdit.py
+++ osu_fusion/modules/mmdit.py
@@ -337,7 +337,7 @@ class MMDiT(nn.Module):
         )
 
         self.final_layer = FinalLayer(dim_h, self.patch_size, dim_h)
-        self.out = nn.Conv1d(dim_h, dim_in_x, 1)
+        self.out = nn.Conv1d(dim_h, dim_in_x, 7, padding=3)
 
         self.initialize_weights()
 
