71cb0aafce25361a110da2e787e7793c53a61d0d
Fauzan Ardhana <fauzanardh91@gmail.com>
2024-08-13 21:29:08 +0700

    remove gca

--- osu_fusion/modules/residual.py
+++ osu_fusion/modules/residual.py
@@ -5,27 +5,6 @@ import torch.nn as nn
 from einops import rearrange
 
 
-class GlobalContext(nn.Module):
-    """Attention-esque squeeze-excite module"""
-
-    def __init__(self: "GlobalContext", dim_in: int, dim_out: int) -> None:
-        super().__init__()
-        self.to_k = nn.Conv1d(dim_in, 1, 1)
-        inner_dim = max(3, dim_out // 2)
-
-        self.layers = nn.Sequential(
-            nn.Conv1d(dim_in, inner_dim, 1),
-            nn.SiLU(),
-            nn.Conv1d(inner_dim, dim_out, 1),
-            nn.Sigmoid(),
-        )
-
-    def forward(self: "GlobalContext", x: torch.Tensor) -> torch.Tensor:
-        context = self.to_k(x)
-        out = torch.einsum("b i d, b j d -> b i j", x, context.softmax(dim=-1))
-        return self.layers(out)
-
-
 class Block(nn.Module):
     def __init__(
         self: "Block",
@@ -74,7 +53,6 @@ class ResidualBlock(nn.Module):
         self.block2 = Block(dim_out, dim_out)
 
         self.res_conv = nn.Conv1d(dim_in, dim_out, 1) if dim_in != dim_out else nn.Identity()
-        self.gca = GlobalContext(dim_out, dim_out)
 
     def forward(
         self: "ResidualBlock",
@@ -92,7 +70,5 @@ class ResidualBlock(nn.Module):
 
         h = self.block1(x, scale_shift=scale_shift)
         h = self.block2(h)
-
-        h = h * self.gca(h)
 
         return h + self.res_conv(x)
