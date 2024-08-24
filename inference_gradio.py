5bf3990a7ba8ae5d548c5bc6a2fe4ec1082e178d
Fauzan Ardhana <fauzanardh91@gmail.com>
2024-08-24 19:08:54 +0700

    change model dim

--- inference_gradio.py
+++ inference_gradio.py
@@ -53,7 +53,7 @@ def create_model_from_checkpoint(
         autoencoder_audio_state_dict = load_file(autoencoder_audio_path)
 
     model_class = DiffusionOsuFusion if model_type == "diffusion" else RectifiedFlowOsuFusion
-    model = model_class(512, attn_infini=False)
+    model = model_class(256, attn_infini=False)
     model.unet.load_state_dict(state_dict)
     model.osu_encoder.load_state_dict(autoencoder_osu_state_dict)
     model.audio_encoder.load_state_dict(autoencoder_audio_state_dict)
