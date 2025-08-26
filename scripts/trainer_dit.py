Re-enable loss masking, and fix gradient checkpointing in mmdit/dit

scripts/trainer_dit.py
@@ -69,12 +69,13 @@ def clear_checkpoints(project_dir: Path) -> None:
 
 def custom_collate_fn(
     batch: List[Tuple[torch.Tensor, torch.Tensor, torch.Tensor]],
-) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    orig_lens = torch.tensor([x.shape[1] for x, _, _ in batch], dtype=torch.int32)
     max_length = max(x.shape[1] for x, _, _ in batch)
     out_x = torch.stack([F.pad(x, (0, max_length - x.shape[1]), value=-1.0) for x, _, _ in batch])
     out_a = torch.stack([F.pad(a, (0, max_length - a.shape[1]), value=0.0) for _, a, _ in batch])
     out_c = torch.stack([c for _, _, c in batch])
-    return out_x, out_a, out_c
+    return out_x, out_a, out_c, orig_lens
 
 
 def visualize_and_log_sample(
@@ -234,7 +235,7 @@ def train(args: ArgumentParser) -> None:  # noqa: C901
         prefetch_factor=4 if args.num_workers > 0 else None,
         persistent_workers=args.num_workers > 0,
         pin_memory=True,
-        collate_fn=custom_collate_fn if args.full_sequence else None,
+        collate_fn=custom_collate_fn,
     )
 
     # Prepare everything with accelerator
@@ -279,10 +280,10 @@ def train(args: ArgumentParser) -> None:  # noqa: C901
             }
 
             for _ in range(args.gradient_accumulation_steps):
-                x, a, c = next(dataloader_cycle)
+                x, a, c, orig_lens = next(dataloader_cycle)
                 with accelerator.autocast(), accelerator.accumulate(model):
                     try:
-                        loss = model(x, a, c)
+                        loss = model(x, a, c, orig_lens)
                     except AssertionError:
                         print(f"AssertionError encountered at step {current_step + 1}, skipping batch.")
                         continue

src/osu_fusion/models/backbone/dit.py
@@ -89,7 +89,7 @@ class DiTBlock(nn.Module):
 
     def forward(self: "DiTBlock", x: torch.Tensor, a: torch.Tensor, c: torch.Tensor) -> torch.Tensor:
         if self.training and self.gradient_checkpointing:
-            return torch.utils.checkpoint.checkpoint(self.forward_body, x, a, c, use_reentrant=False)
+            return torch.utils.checkpoint.checkpoint(self.forward_body, x, a, c, use_reentrant=True)
         else:
             return self.forward_body(x, a, c)
 

src/osu_fusion/models/diffusion.py
@@ -107,6 +107,7 @@ class OsuFusion(nn.Module):
         a_lat: torch.Tensor,
         a_lat_intermediates: Optional[torch.Tensor],
         c_prep: torch.Tensor,
+        orig_lens: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         noise = torch.randn_like(x, device=x.device)
         timesteps = torch.randint(
@@ -123,14 +124,13 @@ class OsuFusion(nn.Module):
         # Calculate loss
         v_target = self.train_scheduler.get_velocity(x, noise, timesteps)
         loss = F.mse_loss(pred, v_target, reduction="none")
-        return loss.mean()
 
-        # # Create mask for losses to ignore padding
-        # if orig_len is not None:
-        #     b, _, n = x.shape
-        #     mask = torch.ones((b, n), device=x.device)
-        #     for i, orig in enumerate(orig_len):
-        #         mask[i, orig:] = 0.0
-        #     mask = repeat(mask, "b n -> b d n", d=BEATMAP_LATENT_DIM)
-        #     return (loss * mask).sum() / mask.sum()
-        # return loss.mean()
+        # Create mask for losses to ignore padding
+        if orig_lens is not None:
+            b, _, n = x.shape
+            mask = torch.ones((b, n), device=x.device)
+            for i, orig in enumerate(orig_lens):
+                mask[i, orig:] = 0.0
+            mask = repeat(mask, "b n -> b d n", d=BEATMAP_DIM)
+            return (loss * mask).sum() / mask.sum()
+        return loss.mean()

src/osu_fusion/models/rectified_flow.py
@@ -95,7 +95,7 @@ class OsuFusion(nn.Module):
         x: torch.Tensor,
         a_lat: torch.Tensor,
         c_prep: torch.Tensor,
-        orig_len: Optional[torch.Tensor] = None,
+        orig_lens: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         noise = torch.randn_like(x, device=x.device)
         timestep = torch.rand(x.shape[0], device=x.device)
@@ -109,14 +109,13 @@ class OsuFusion(nn.Module):
 
         # Calculate loss
         loss = F.mse_loss(pred, flow, reduction="none")
-        return loss.mean()
 
-        # # Create mask for losses to ignore padding
-        # if orig_len is not None:
-        #     b, _, n = x.shape
-        #     mask = torch.ones((b, n), device=x.device)
-        #     for i, orig in enumerate(orig_len):
-        #         mask[i, orig:] = 0.0
-        #     mask = repeat(mask, "b n -> b d n", d=BEATMAP_DIM)
-        #     return (loss * mask).sum() / mask.sum()
-        # return loss.mean()
+        # Create mask for losses to ignore padding
+        if orig_lens is not None:
+            b, _, n = x.shape
+            mask = torch.ones((b, n), device=x.device)
+            for i, orig in enumerate(orig_lens):
+                mask[i, orig:] = 0.0
+            mask = repeat(mask, "b n -> b d n", d=BEATMAP_DIM)
+            return (loss * mask).sum() / mask.sum()
+        return loss.mean()